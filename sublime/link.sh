5c0a338efdb82b0d52e7b0e7e15118c5693d16df
Brev Patterson <me@brev.name>
2013-12-15 11:29:36 -0800

    fixes

--- sublime/link.sh
+++ sublime/link.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-ln -fs $PWD/Preferences.sublime-settings /Users/bpatterson/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -fs $PWD/Preferences.sublime-settings $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+sudo ln -sf /Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+
