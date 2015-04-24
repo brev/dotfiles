839c3303b9acf269de4a44f8d360e37faf6d4f5c
Brev Patterson <me@brev.name>
2015-04-24 15:21:07 -0700

    more

--- atom/link.sh
+++ atom/link.sh
@@ -3,11 +3,9 @@ if [ ! -d "$HOME/.atom/" ]; then
 	mkdir -p $HOME/.atom/ || echo 'Mkdir failed'
 fi
 
-if [ -e "$HOME/.atom/config.cson" ]; then
-	echo "File already exists cannot symlink"
-	exit 1
-fi
-
-ln -fs $PWD/config.cson $HOME/.atom/config.cson || echo 'Symlink failed'
+for FILE in config.cson
+do
+	ln -fs $PWD/$FILE $HOME/.atom/$FILE || echo 'Symlink failed'
+done
 
 exit 0
