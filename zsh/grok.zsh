45f13e4c8cccb482a11408072eab614677480bcf
Brev Patterson <me@brev.name>
2015-09-27 12:12:53 -0700

    nupic dev workflow.

--- zsh/grok.zsh
+++ zsh/grok.zsh
@@ -1,3 +1,2 @@
 export APPLICATION_CONFIG_PATH=$HOME/src/products/grok/conf
 export GROK_CONFIG_PATH=$HOME/src/products/grok/conf
-export PYTHONPATH=$PYTHONPATH:$HOME/Library/Python/2.7/lib/python/site-packages/
--- zsh/nupic.zsh
+++ zsh/nupic.zsh
@@ -1,9 +1,13 @@
-export NTA=$HOME/Library/nupic
-export NTA_ROOT_DIR=$NTA
 export NUPIC=$HOME/src/nupic
-export PYTHONPATH=$PYTHONPATH:$NTA/lib/python2.7/site-packages:/usr/local/lib/python2.7/site-packages
 
-export ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future
+#export ARCHFLAGS=-Wno-error=unused-command-line-argument-hard-error-in-future
+export ARCHFLAGS="-arch x86_64"
 export CFLAGS=-Qunused-arguments
 export CPPFLAGS=-Qunused-arguments
 export MACOSX_DEPLOYMENT_TARGET=10.10
+
+# https://github.com/numenta/nupic/wiki/Numenta-Developer-Setup
+export PATH=${HOME}/Library/Python/local:${PATH}
+export PYTHONPATH=${HOME}/Library/Python/local/lib/python2.7/site-packages:${PYTHONPATH}
+export DYLD_LIBRARY_PATH=${HOME}/Library/Python/local/lib:${DYLD_LIBRARY_PATH}
+export MANPATH=${HOME}/Library/Python/local/man
